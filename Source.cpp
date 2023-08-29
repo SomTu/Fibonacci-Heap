ba846262bdf408e212c46a9d3b22fe0990f02a64
SomTu <30876455+SomTu@users.noreply.github.com>
2023-08-29 12:13:20 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -14,18 +14,6 @@ int main() {
 	while (!halda.empty()) {
 		std::cout << halda.extract_min() << "\n";
 	}
-
-
-	//std::vector<std::unique_ptr<fib::Node<char>>> vec;
-	//for (int i = 0; i < 7; i++) {
-	//	vec.push_back(nullptr);
-	//	std::unique_ptr<fib::Node<char>> ptr = std::make_unique<fib::Node<char>>('a' + i, i);
-	//	fib::merge_in_vector<char>(vec, std::move(ptr));
-	//	// 0 -> 1 dissapears
-	//}
-
-
-
-
+	
 	return 0;
 }
